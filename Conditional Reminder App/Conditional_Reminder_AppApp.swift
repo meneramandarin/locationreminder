84e39324cb86f4f326bfd4a954455cacb7cdee04
Your Name <you@example.com>
2024-01-15 13:02:33 -0800

    Initial Commit

---
+++ Conditional Reminder App/Conditional_Reminder_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conditional_Reminder_AppApp.swift
+//  Conditional Reminder App
+//
+//  Created by Marlene on 15.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Conditional_Reminder_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Conditional Reminder App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Conditional Reminder App
+//
+//  Created by Marlene on 15.01.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
