b0ee7ffe56b18f9deacd96e027b29369b46111ad
Your Name <marlene.ronstedt@gmail.com>
2024-02-16 13:56:24 -0800

    location pin on contentview

--- Conditional Reminder App/ContentView.swift
+++ Conditional Reminder App/ContentView.swift
@@ -1,4 +1,4 @@
-/
+//
 //  ContentView.swift
 //  Conditional Reminder App
 //
@@ -9,122 +9,117 @@ import MapKit
 import SwiftUI
 
 struct MapView: UIViewRepresentable {
-  @Binding var region: MKCoordinateRegion
+    var region: MKCoordinateRegion
+    var annotations: [MKPointAnnotation]
 
-  func makeUIView(context: Context) -> MKMapView {
-    let mapView = MKMapView()
-    mapView.setRegion(region, animated: true)
-    return mapView
-  }
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.setRegion(region, animated: true)
+        return mapView
+    }
 
-  func updateUIView(_ uiView: MKMapView, context: Context) {
-    uiView.setRegion(region, animated: true)
-  }
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        uiView.setRegion(region, animated: true)
+        uiView.removeAnnotations(uiView.annotations) // Clear existing annotations
+        uiView.addAnnotations(annotations) // Add new annotations
+    }
 }
 
 struct ContentView: View {
-  @State private var reminders: [Reminder] = []  // State variable for reminders
-  private let reminderStorage = ReminderStorage(
-    context: PersistenceController.shared.container.viewContext)
+    @State private var reminders: [Reminder] = [] // State variable for reminders
+    private let reminderStorage = ReminderStorage(
+        context: PersistenceController.shared.container.viewContext)
 
     var body: some View {
         NavigationView {
             ZStack {
                 Color(hex: "023020").edgesIgnoringSafeArea(.all)
 
-                GeometryReader { geometry in // GeometryReader to read the view's size
+                GeometryReader { geometry in
                     ScrollView {
                         VStack {
-                            Spacer()
-                                .frame(height: geometry.size.height / 5) // Push content to 1/4rd of the view's height
+                            Spacer().frame(height: geometry.size.height / 5)
                             
                             NavigationLink(destination: SetReminderView()) {
                                 Text("New Memory")
                                     .padding()
                                     .padding(.vertical, 10)
-                                    .frame(width: UIScreen.main.bounds.width * 0.5) // Set the width to half of the screen's width
+                                    .frame(width: UIScreen.main.bounds.width * 0.5)
                                     .adaptiveFont(name: "Times New Roman", style: .headline)
-                                    .background(Color(hex: "FEEBCC")) // Beige color
-                                    .foregroundColor(Color(hex: "023020")) // Dark green color
+                                    .background(Color(hex: "FEEBCC"))
+                                    .foregroundColor(Color(hex: "023020"))
                                     .cornerRadius(110)
                             }
                 
-                            Spacer()
-                                .frame(height: geometry.size.height / 4) // Second spacer to push "Your Memories:" to the lower third. man this is so bad because it's hardcoded and looks cute on my iPhone mini but not elsewhere. need to achieve this rather like this: .padding(.top, geometry.size.height * 0.1) and maybe put the reminders below into their own vstack.
+                            Spacer().frame(height: geometry.size.height / 4)
                 
-                Text("Your Memories:")
-                    .adaptiveFont(name: "Times New Roman", style: .headline)
-                    .foregroundColor(Color(hex: "FEEBCC")) // beige
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .padding(.horizontal)
+                            Text("Your Memories:")
+                                .adaptiveFont(name: "Times New Roman", style: .headline)
+                                .foregroundColor(Color(hex: "FEEBCC"))
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.horizontal)
                 
-                // Iterate over the reminders
-                ForEach(reminders, id: \.id) { reminder in
-                    VStack(alignment: .leading) {
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(reminder.message)
-                                    .font(.headline)
-                                    .foregroundColor(Color(hex: "FEEBCC")) // beige
-                                Text(reminder.date, style: .date)
-                                    .font(.subheadline)
-                                    .foregroundColor(Color(hex: "FEEBCC"))
-                            }
-                            Spacer()
-                            Button(action: {
-                                print("Delete button tapped for reminder with ID: \(reminder.id)")
-                                // Action to delete a reminder
-                                reminderStorage.deleteReminder(reminder)
-                                // Reload reminders to reflect the change
-                                loadReminders()
-                            }) {
-                                Image(systemName: "xmark.circle")
-                                    .foregroundColor(Color(hex: "#F4C2C2")) // rose / pink
+                            ForEach(reminders, id: \.id) { reminder in
+                                VStack(alignment: .leading) {
+                                    HStack {
+                                        VStack(alignment: .leading) {
+                                            Text(reminder.message)
+                                                .font(.headline)
+                                                .foregroundColor(Color(hex: "FEEBCC"))
+                                            Text(reminder.date, style: .date)
+                                                .font(.subheadline)
+                                                .foregroundColor(Color(hex: "FEEBCC"))
+                                        }
+                                        Spacer()
+                                        Button(action: {
+                                            reminderStorage.deleteReminder(reminder)
+                                            loadReminders()
+                                        }) {
+                                            Image(systemName: "xmark.circle")
+                                                .foregroundColor(Color(hex: "#F4C2C2"))
+                                        }
+                                    }
+                                    
+                                    MapView(region: region(for: reminder), annotations: [createAnnotation(for: reminder)])
+                                        .frame(height: 200)
+                                        .cornerRadius(10)
+                                }
+                                .padding()
+                                .background(Color.gray.opacity(0.2))
+                                .cornerRadius(10)
+                                .padding(.horizontal)
                             }
                         }
-                        
-                        // Map view for each reminder
-                        MapView(region: .constant(region(for: reminder)))
-                            .frame(height: 200)
-                            .cornerRadius(10)
                     }
-                    .padding()
-                    .background(Color.gray.opacity(0.2))
-                    .cornerRadius(10)
-                    .padding(.horizontal)
                 }
             }
+            .onAppear {
+                loadReminders()
+            }
         }
-        }
-        }
-      .onAppear {
-        loadReminders()
-      }
     }
-  }
 
     private func loadReminders() {
-        do {
-            reminders = try reminderStorage.fetchReminders()
-            for reminder in reminders {
-                print("Loaded reminder with ID: \(reminder.id)")
-            }
-        } catch {
-            print("Failed to load reminders: \(error)")
-        }
+        reminders = try! reminderStorage.fetchReminders()
     }
 
-  private func region(for reminder: Reminder) -> MKCoordinateRegion {
-    // Create an MKCoordinateRegion based on the reminder's location
-    MKCoordinateRegion(
-      center: reminder.location,
-      span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
-    )
-  }
+    private func region(for reminder: Reminder) -> MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: reminder.location,
+            span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
+        )
+    }
+
+    private func createAnnotation(for reminder: Reminder) -> MKPointAnnotation {
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = reminder.location
+        annotation.title = reminder.message
+        return annotation
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
