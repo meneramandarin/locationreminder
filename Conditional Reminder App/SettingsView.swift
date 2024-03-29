24b255c14eea768cca9e92896e90d14c999bf309
Your Name <marlene.ronstedt@gmail.com>
2024-03-28 22:33:15 -0700

    hotspot page is soooo pretty now :')

--- Conditional Reminder App/SettingsView.swift
+++ Conditional Reminder App/SettingsView.swift
@@ -5,92 +5,117 @@
 //  Created by Marlene on 06.03.24.
 //
 
-import SwiftUI
 import MapKit
+import SwiftUI
 
 struct HotspotView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-    @Environment(\.presentationMode) var presentationMode
-    @State private var hotspotName: String = ""
-    @State private var hotspots: [Hotspot] = []
-    let reminderStorage: ReminderStorage
-    
-    init(reminderStorage: ReminderStorage) {
-        self.reminderStorage = reminderStorage
-        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color(hex: "FEEBCC"))]
-    }
-    
-    var body: some View {
-        NavigationStack {
-        ZStack {
-            Color(hex: "023020").edgesIgnoringSafeArea(.all)
-            
-            ScrollView {
+  @Environment(\.managedObjectContext) private var viewContext
+  @Environment(\.presentationMode) var presentationMode
+  @State private var hotspotName: String = ""
+  @State private var hotspots: [Hotspot] = []
+  let reminderStorage: ReminderStorage
+
+  init(reminderStorage: ReminderStorage) {
+    self.reminderStorage = reminderStorage
+    UINavigationBar.appearance().titleTextAttributes = [
+      .foregroundColor: UIColor(Color(hex: "FEEBCC"))
+    ]
+  }
+
+  var body: some View {
+    NavigationStack {
+      ZStack {
+        Color(hex: "023020").edgesIgnoringSafeArea(.all)
+
+        ScrollView {
+          VStack {
+            Text(
+              "Hotspots are places you visit frequently, like your work place or your home. Set up a Hotspot to be able to refer to these places by their name instead of their address."
+            )
+            .font(.footnote)
+            .foregroundColor(Color(hex: "FEEBCC"))
+            .padding(.bottom)
+
+            Spacer()
+
+            VStack {
+              ForEach(hotspots.indices, id: \.self) { index in
                 VStack {
-                    Text("Hotspots are places you revisit a lot and to which you would rather like to refer by saying “Home” or “Work” instead of repeating the actual address.")
-                        .font(.footnote)
-                        .foregroundColor(Color(hex: "FEEBCC"))
-                    
+                  HStack {
+
+                    Text(hotspots[index].name)
+                      .foregroundColor(Color(hex: "023020"))
+                      .padding()
+
                     Spacer()
-                    
-                    ForEach(hotspots) { hotspot in
-                        HStack {
-                            Text(hotspot.name)
-                                .foregroundColor(Color(hex: "023020"))
-                            
-                            Spacer()
-                            
-                            Button(action: {
-                                reminderStorage.deleteHotspot(hotspot)
-                                loadHotspots()
-                            }) {
-                                Image(systemName: "xmark.circle")
-                                    .foregroundColor(Color(hex: "#F4C2C2"))
-                            }
-                        }
-                        .background(Color(hex: "FEEBCC"))
-                        .cornerRadius(8)
+
+                    Button(action: {
+                      reminderStorage.deleteHotspot(hotspots[index])
+                      loadHotspots()
+                    }) {
+                      Image(systemName: "xmark.circle")
+                        .foregroundColor(Color(hex: "#F4C2C2"))
                     }
-                    
-                    Spacer ()
-                
-                        NavigationLink(destination: NewHotspot(reminderStorage: reminderStorage)) {
-                  
-                        HStack {
-                            Text("Add Hotspot")
-                            Image(systemName: "arrow.right")
-                        }
-                        .font(.headline)
-                        .foregroundColor(Color(hex: "#FFBF00"))
-                    
-                    }
-                    }
+
+                    Spacer()
+                      .frame(width: 10)
+
+                  }
+
+                  if index < hotspots.count - 1 {
+                    Rectangle()
+                      .fill(Color.brown)
+                      .frame(height: 1)
+                      .padding([.leading, .trailing], 15)
+                  }
                 }
-                .padding()
+              }
             }
-        }
-        .navigationBarTitle("Hotspots", displayMode: .inline) // Set the title
-        .navigationBarBackButtonHidden(true)
-        .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button(action: {
-                    presentationMode.wrappedValue.dismiss()
-                }) {
-                     HStack(spacing: 4) {
-                        Image(systemName: "chevron.left") // Back arrow for visual clarity
-                        Text("Menu")
-                    }
-                    .foregroundColor(Color(hex: "#FFBF00"))  // Customize Menu color
-                }
+            .background(Color(hex: "FEEBCC"))
+            .cornerRadius(8)
+
+            Spacer()
+
+            NavigationLink(destination: NewHotspot(reminderStorage: reminderStorage)) {
+
+              HStack {
+                Text("Add Hotspot")
+                Image(systemName: "arrow.right")
+              }
+              .font(.headline)
+              .foregroundColor(Color(hex: "#FFBF00"))
+              .padding(.top)
+
             }
+          }
         }
-        .onAppear {
-            loadHotspots()
-            UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color(hex: "FEEBCC"))]
+        .padding()
+      }
+    }
+    .navigationBarTitle("Hotspots", displayMode: .inline)  // Set the title
+    .navigationBarBackButtonHidden(true)
+    .toolbar {
+      ToolbarItem(placement: .navigationBarLeading) {
+        Button(action: {
+          presentationMode.wrappedValue.dismiss()
+        }) {
+          HStack(spacing: 4) {
+            Image(systemName: "chevron.left")  // Back arrow for visual clarity
+            Text("Menu")
+          }
+          .foregroundColor(Color(hex: "#FFBF00"))  // Customize Menu color
         }
+      }
     }
-    
-    private func loadHotspots() {
-        hotspots = reminderStorage.fetchHotspots()
+    .onAppear {
+      loadHotspots()
+      UINavigationBar.appearance().titleTextAttributes = [
+        .foregroundColor: UIColor(Color(hex: "FEEBCC"))
+      ]
     }
+  }
+
+  private func loadHotspots() {
+    hotspots = reminderStorage.fetchHotspots()
+  }
 }
